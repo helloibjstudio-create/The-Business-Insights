8d4916b649bcbb5492c45704f3b9255804bba137
helloibjstudio-create <helloibj.studio@gmail.com>
2025-11-14 19:06:46 +0100

    Remove commented-out middleware code

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,1 @@
-// import { NextRequest, NextResponse } from "next/server";
 
-// export function middleware(req: NextRequest) {
-//   const host = req.headers.get("host");
-
-//   // your production domain
-//   const productionDomain = "the-business-insights.vercel.app";
-
-//   if (host === productionDomain) {
-//     // Rewrite ALL pages → maintenance page
-//     return NextResponse.rewrite(new URL("/maintenance", req.url));
-//   }
-
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: "/:path*", // apply to all pages
-// };
