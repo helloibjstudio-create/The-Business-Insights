f775fb396d0b66ae99727ea2c6be7f1761458dce
helloibjstudio-create <helloibj.studio@gmail.com>
2025-12-22 14:42:24 +0100

    Pulled and now pushing

--- app/components/Dhl.tsx
+++ app/components/Dhl.tsx
@@ -4,10 +4,9 @@ import { dhl, thirdOrange } from "@/public";
 const Dhl = () => {
   return (
     <div>
-      <a href="https://www.dhl.com/us-en/home/innovation-in-logistics/innovation-center-middle-east-and-africa.html">
         <div
           key="dhl-banner"
-          className="relative w-full min-h-[50vh] sm:h-[70vh] md:h-screen mb-20 md:mb-0"
+          className="relative w-full min-h-[10vh] sm:h-[40vh] lg:h-screen mb-20 md:mb-0"
         >
           <Image
             src={thirdOrange}
@@ -15,15 +14,16 @@ const Dhl = () => {
             fill
             className="absolute inset-0 object-contain lg:object-cover h-full w-full"
           />
+      <a href="https://www.dhl.com/us-en/home/innovation-in-logistics/innovation-center-middle-east-and-africa.html">
           <Image
             src={dhl}
             alt="DHL Banner"
             width={1280}
             height={333}
             className="z-10 absolute top-2/5 left-1/2 -translate-x-1/2 -translate-y-1/2 object-contain w-[90%] sm:w-[80%] lg:w-[90%]"
-          />
+            />
+            </a>
         </div>
-      </a>
     </div>
   );
 };
